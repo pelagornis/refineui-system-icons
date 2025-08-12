c9fc08817a232a8ea00cb707fb2d6d79ae68c60d
oniixxx <oniix.dev@gmail.com>
2025-08-13 03:24:54 +0900

    Reset Assets

---
+++ android/app/src/main/java/com/pelagornis/refineui/IconInfo.kt
@@ -0,0 +1,3 @@
+package com.pelagornis.refineui
+
+data class IconInfo()
---
+++ android/app/src/main/java/com/pelagornis/refineui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.pelagornis.refineui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pelagornis.refineui_system_icons.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
