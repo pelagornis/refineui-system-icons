57ee56fc7091bccc8eda0ed867ea32534101f687
jihoonahn <jihoonahn.dev@gmail.com>
2025-09-06 02:52:32 +0900

    Update ios package setup

--- Package.swift
+++ Package.swift
@@ -1,17 +1,24 @@
-// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
     name: "RefineIcons",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "RefineIcons",
-            targets: ["RefineIcons"]),
+            targets: ["RefineIcons"]
+        )
     ],
     targets: [
         .target(
             name: "RefineIcons",
-            path: "ios/RefineIcons"
+            path: "ios/RefineIcons/Sources",
+            resources: [
+                .process("ios/RefineIcons/Resources")
+            ]
         )
     ]
 )
