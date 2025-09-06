44f5312de9df8f87c984c51a9070ba849dd70071
GitHub Action <action@github.com>
2025-09-06 17:15:38 +0000

    chore(android): bump version to 0.1.30
    - Update android/library/build.gradle.kts versionName field
    - Triggered by tag: refs/tags/0.1.30
    - Workflow: Release Android Platform
    - Commit: 52c815871ea76038169745b9de6cee85bb4d4d7d

--- android/library/build.gradle.kts
+++ android/library/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
         versionCode = 1
-        versionName = "0.1.29"
+        versionName = "0.1.30"
     }
 
     buildTypes {
