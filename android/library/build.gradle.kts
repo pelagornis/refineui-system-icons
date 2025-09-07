cd37b07bd51e7e51de90da5255efb43430489a33
GitHub Action <action@github.com>
2025-09-07 10:21:29 +0000

    chore(android): bump version to 0.1.44
    - Update android/library/build.gradle.kts versionName field
    - Triggered by tag: refs/tags/0.1.44
    - Workflow: Release Android Platform
    - Commit: c7996a1a8919695f11611be858e807045d04f29f

--- android/library/build.gradle.kts
+++ android/library/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
         versionCode = 1
-        versionName = "0.1.43"
+        versionName = "0.1.44"
     }
 
     buildTypes {
