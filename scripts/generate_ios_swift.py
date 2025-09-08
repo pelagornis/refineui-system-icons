60cf76f80b8ec94bf9320100c064f8db47ae14cd
jihoonahn <jihoonahn.dev@gmail.com>
2025-09-08 20:52:49 +0900

    Update ios Swift Script

--- scripts/generate_ios_swift.py
+++ scripts/generate_ios_swift.py
@@ -56,7 +56,12 @@ def generate_ios_swift():
 def generate_refine_icons_swift(sources_dir: Path, icon_cases: list):
     """Generates RefineIcons.swift file in RefineUI style."""
     
-    swift_content = """import Foundation
+    swift_content = """//
+//  Copyright (c) Pelagornis.
+//  Licensed under the MIT license.
+//
+
+import Foundation
 
 /// RefineUI System Icons
 @objc public enum RefineUIIcons: Int, Equatable, CaseIterable {
@@ -107,40 +112,86 @@ def convert_to_camel_case(icon_case: str) -> str:
 def generate_extensions_swift(sources_dir: Path):
     """Generates RefineIcons+Extensions.swift file."""
     
-    swift_content = """import SwiftUI
+    swift_content = """//
+//  Copyright (c) Pelagornis.
+//  Licensed under the MIT license.
+//
 
-// MARK: - Bundle Helper
-extension Bundle {
-    /// The bundle for RefineUIIcons resources
-    static var refineUIIcons: Bundle {
-        #if SWIFT_PACKAGE
-        return Bundle.module
-        #else
-        return Bundle(for: RefineUIIcons.self)
-        #endif
-    }
-}
+import SwiftUI
 
 // MARK: - SwiftUI Extensions
 extension Image {
     /// Create an image from RefineUI icon
     /// - Parameter icon: The icon to display
     /// - Returns: SwiftUI Image
-    public init(refineIcon icon: RefineUIIcons) {
-        self.init(icon.resourceString, bundle: Bundle.refineUIIcons)
+    public init(refineUIIcon: RefineUIIcons) {
+        #if os(macOS)
+        self.init(nsImage: NSImage.refineUIIcon(refineUIIcon))
+        #else
+        self.init(uiImage: UIImage(refineUIIcon: refineUIIcon))
+        #endif
     }
 }
 
-// MARK: - UIKit Extensions
-#if canImport(UIKit)
+#if os(macOS)
+import AppKit
+
+private class RefineUIIconsBundleCheck {}
+
+public extension NSImage {
+#if REFINE_UI_ICONS_USE_RESOURCE_BUNDLES
+    @objc static let refineUIIconBundle = Bundle(path: Bundle(for: RefineUIIconsBundleCheck.self).path(forResource: "RefineUIIcons", ofType: "bundle")!)
+#else
+    @objc static let refineUIIconBundle = Bundle(for: RefineUIIconsBundleCheck.self)
+#endif
+    
+    @objc static func refineUIIcon(_ refineUIIcon: RefineUIIcons) -> NSImage {
+        // Force unwrap here because the resource strings
+        // are generated so we can be confident that the image
+        // exits at runtime.
+#if SWIFT_PACKAGE
+        return Bundle.module.image(forResource: NSImage.Name(refineUIIcon.resourceString))!
+#else
+        return NSImage.refineUIIconBundle.image(forResource: NSImage.Name(refineUIIcon.resourceString))!
+#endif
+    }
+}
+
+#endif
+
+#if os(iOS)
 import UIKit
 
-extension UIImage {
-    /// Create an image from RefineUI icon
-    /// - Parameter icon: The icon to display
-    /// - Returns: UIImage
-    public convenience init?(refineIcon icon: RefineUIIcons) {
-        self.init(named: icon.resourceString, in: Bundle.refineUIIcons, compatibleWith: nil)
+private class RefineUIIconsBundleCheck {}
+
+public extension UIImage {
+#if REFINE_UI_ICONS_USE_RESOURCE_BUNDLES
+    @objc static let refineUIIconBundle = Bundle(path: Bundle(for: RefineUIIconsBundleCheck.self).path(forResource: "RefineUIIcons", ofType: "bundle")!)
+#else
+    @objc static let refineUIIconBundle = Bundle(for: RefineUIIconsBundleCheck.self)
+#endif
+    
+    @objc convenience init(refineUIIcon: RefineUIIcons) {
+        // Force unwrap here because the resource strings
+        // are generated so we can be confident that the image
+        // exits at runtime.
+#if SWIFT_PACKAGE
+        self.init(named: refineUIIcon.resourceString, in: Bundle.module, compatibleWith: nil)!
+#else
+        self.init(named: refineUIIcon.resourceString, in: UIImage.refineUIIconBundle, compatibleWith: nil)!
+#endif
+    }
+}
+
+public extension UIImageView {
+    @objc convenience init(refineUIIcon: RefineUIIcons, tintColor: UIColor) {
+        self.init(image: UIImage(refineUIIcon: refineUIIcon))
+        self.tintColor = tintColor
+    }
+    
+    @objc convenience init(refineUIIcon: RefineUIIcons, highlightedRefineUIIcon: RefineUIIcons, tintColor: UIColor) {
+        self.init(image: UIImage(refineUIIcon: refineUIIcon), highlightedImage: UIImage(refineUIIcon: highlightedRefineUIIcon))
+        self.tintColor = tintColor
     }
 }
 #endif
