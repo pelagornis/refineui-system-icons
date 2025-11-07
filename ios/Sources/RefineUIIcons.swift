1c3cae83533201b7738438d0fbd12e305554e49b
jihoonahn <jihoonahn.dev@gmail.com>
2025-11-08 02:39:47 +0900

    Update Swift Version 5.10 -> 6.0

--- ios/Sources/RefineUIIcons.swift
+++ ios/Sources/RefineUIIcons.swift
@@ -6,7 +6,7 @@
 import Foundation
 
 /// RefineUI System Icons
-@objc public enum RefineUIIcons: Int, Equatable, CaseIterable {
+@objc public enum RefineUIIcons: Int, Sendable, Equatable, CaseIterable {
     case accessTime16Filled = 0
     case accessTime16Regular = 1
     case accessTime20Filled = 2
