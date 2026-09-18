20adacc0e81d6125eaa76a05affb50ac4f28b644
GitHub Action <action@github.com>
2026-09-18 09:36:41 +0000

    chore(android): bump version to 0.3.42
      - Update android/gradle.properties VERSION_NAME field
      - Triggered by tag: refs/tags/0.3.42
      - Workflow: Release Android Platform
      - Commit: 1046ca5f1fd552e057c030238da633cef6e14b89

--- android/library/build.gradle.kts
+++ android/library/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = findProperty("GROUP") as String? ?: "com.pelagornis"
-version = findProperty("VERSION_NAME") as String? ?: "0.3.41"
+version = findProperty("VERSION_NAME") as String? ?: "0.3.42"
 
 android {
     namespace = "com.pelagornis.refineui.icons"
